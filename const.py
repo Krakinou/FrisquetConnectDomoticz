0fd0608af757e3803a0967fc2878751e2e291f00
Krakinou <misterl56@hotmail.com>
2025-12-21 11:22:35 +0100

    Initial Commit

---
+++ const.py
@@ -0,0 +1,11 @@
+"""Constantes pour intégration Frisquet-Connect """
+
+HOST="fcutappli.frisquet.com"
+AUTH_API="/api/v1/authentifications"
+SITE_API="/api/v1/sites"
+ORDRES_API="/api/v1/ordres"
+
+C_TAMB="1"
+C_CONS_CONF="2"
+C_CONS_RED="3"
+C_CONS_HG="4"
